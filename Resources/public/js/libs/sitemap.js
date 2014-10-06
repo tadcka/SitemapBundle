a0bc7c6dcbb2e4219af63b49f71a679d6e77d445
Tadas <tadcka89@gmail.com>
2014-10-06 23:28:18 +0300

    Revert

--- Resources/public/js/libs/sitemap.js
+++ Resources/public/js/libs/sitemap.js
@@ -15,7 +15,7 @@ $.fn.sitemap = function () {
 
     $tree.getJsTree()
         .on('changed.jstree', function ($event, $data) {
-            if ($data.node) {
+            if (!$currentNode || $data.node && (($currentNode.id !== $data.node.id))) {
                 $currentNode = $data.node;
                 var $url = Routing.generate('tadcka_sitemap_content', {_format: 'json', nodeId: $currentNode.id});
 
